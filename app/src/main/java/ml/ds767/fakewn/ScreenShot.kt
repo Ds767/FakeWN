14c2415b2008c992bf06a2adb6da2ae59fccfcda
Ds767 <15981920968@163.com>
2019-08-15 22:41:42 +0800

    日期
    进度条上日期

--- app/src/main/java/ml/ds767/fakewn/ScreenShot.kt
+++ app/src/main/java/ml/ds767/fakewn/ScreenShot.kt
@@ -2,7 +2,13 @@ package ml.ds767.fakewn
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.SystemClock
 import android.widget.TextView
+import java.sql.Date
+import java.sql.Time
+import java.text.SimpleDateFormat
+import java.util.*
+import java.util.logging.SimpleFormatter
 
 class ScreenShot : AppCompatActivity() {
 
@@ -11,10 +17,36 @@ class ScreenShot : AppCompatActivity() {
         setContentView(R.layout.activity_screen_shot)
 
         val scoreArr=intent.getIntArrayExtra(EXTRA_ARRAY)
+//日期格式等
+        val sdf=SimpleDateFormat("yyyy.MM.dd")
+        val sdfmd=SimpleDateFormat("MM.dd")
+
+        val today=Date(System.currentTimeMillis())
+        val ymd=sdf.format(today)
+//当日日期
+        findViewById<TextView>(R.id.Time).apply { text=ymd}
+//进度条上日期
+        val cal= Calendar.getInstance()
+        cal.setTime(today)
+        cal.add(Calendar.DAY_OF_MONTH,-1)
+        findViewById<TextView>(R.id.date1).apply { text=sdfmd.format(cal.time)}
+        cal.add(Calendar.DAY_OF_MONTH,-1)
+        findViewById<TextView>(R.id.date2).apply { text=sdfmd.format(cal.time)}
+        cal.add(Calendar.DAY_OF_MONTH,-1)
+        findViewById<TextView>(R.id.date3).apply { text=sdfmd.format(cal.time)}
+        cal.add(Calendar.DAY_OF_MONTH,-1)
+        findViewById<TextView>(R.id.date4).apply { text=sdfmd.format(cal.time)}
+        cal.add(Calendar.DAY_OF_MONTH,-1)
+        findViewById<TextView>(R.id.date5).apply { text=sdfmd.format(cal.time)}
+        cal.add(Calendar.DAY_OF_MONTH,-1)
+        findViewById<TextView>(R.id.date6).apply { text=sdfmd.format(cal.time)}
+        cal.add(Calendar.DAY_OF_MONTH,-1)
+        findViewById<TextView>(R.id.date7).apply { text=sdfmd.format(cal.time)}
+
 
     }
 
-    //val textTime=findViewById<TextView>(R.id.Time).apply{}
+
     //val textScore=findViewById<TextView>(R.id.Score).apply {}
 
 
